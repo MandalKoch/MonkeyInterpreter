210bfc8ca7d0743e1d8c4afbd0eb03e1fe5e3d59
Andreas Koch <andreas.Koch2010@web.de>
2024-02-18 11:03:32 +0100

    Adding Program.cs to main

---
+++ MonkeyInterpreter.Main/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var userName = Environment.UserName;
+Console.WriteLine($"Hello Mister {userName}! This is the Monkey programming language!");
